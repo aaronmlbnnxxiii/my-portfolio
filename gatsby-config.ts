bd3fbd3a368434f963d0ca1862a5402b91719cb3
Aaron Jay Malabanan <malabanan.aaronjay@gmail.com>
2023-02-03 23:02:30 +0800

    remove preview

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,14 +24,6 @@ const config: GatsbyConfig = {
         customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
       },
     },
-    {
-      resolve: "gatsby-plugin-prismic-previews",
-      options: {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-      },
-    },
     // "gatsby-plugin-netlify-cms",
     "gatsby-plugin-postcss",
     `gatsby-plugin-image`,
